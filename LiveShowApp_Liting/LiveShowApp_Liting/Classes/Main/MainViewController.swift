dd79160bbe58a4bb2642ec147e4dbad2397244fd
wanglt311 <wanglt311@gamil.com>
2018-06-18 23:27:26 +0800

    add controller and init constructor and add btnView

---
+++ LiveShowApp_Liting/LiveShowApp_Liting/Classes/Main/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  LiveShowApp_Liting
+//
+//  Created by Liting Wang on 6/18/18.
+//  Copyright © 2018 Liting Wang. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LiveShowApp_Liting/LiveShowApp_Liting/Classes/Tools/Extension/UIBarButtonItem-Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIBarButtonItem-Extension.swift
+//  LiveShowApp_Liting
+//
+//  Created by Liting Wang on 6/18/18.
+//  Copyright © 2018 Liting Wang. All rights reserved.
+//
+
+import Foundation
